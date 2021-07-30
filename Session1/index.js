211e0c837fc4e551845d8838ac47084e703214ba
kraken9x <47894224+kraken9x@users.noreply.github.com>
2021-07-30 21:47:00 +0700

    Update file

---
+++ Session1/index.js
@@ -0,0 +1,59 @@
+// console.log("Hello world");
+
+// 1. Khai báo biến
+// let, const
+
+// let name = "Peter";
+// console.log(name);
+// name = "Tony";
+// console.log(name);
+
+// const age = 18;
+// console.log(age);
+// age = 19;
+// console.log(age);
+
+// 2. In ra màn hình console
+// console.log("Hello everyone");
+
+// 3. Nhập dữ liệu từ bàn phím
+// let userInput = prompt("Please input your name?");
+// console.log(userInput);
+
+// Ex1:
+// Cho người dùng nhập vào 2 số a, b => Tính
+// tổng a + b và in ra màn hình console
+
+// let a = prompt("Please input a");
+// let b = prompt("Please input b");
+
+// a = Number(a);
+// b = Number(b);
+
+// let sum = a + b;
+// console.log(sum);
+
+// String, Number (Primitive - Nguyên thuỷ)
+
+// 4. String (Chuỗi)
+// camelCase
+
+let fullName = "Peter Parker";
+
+console.log(fullName);
+
+// Nối chuỗi - concatenation (+)
+console.log("Hello " + fullName);
+
+// Template string
+let randomQuote = `Xin chào, tên tôi là ${fullName}.
+Năm nay tôi 30 tuổi`;
+console.log(randomQuote);
+
+// 5. Number (Số)
+let a = 12;
+let b = 13;
+
+// Toán tử (+, -, *, /, %, **);
+
+console.log(a ** b);
